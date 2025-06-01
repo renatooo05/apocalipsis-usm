5d0ac5ff1f8e5a74dbf40d866ed32b69baf969f3
L0k0tr0n <darmijo@usm.cl>
2025-06-01 16:05:15 -0400

    Funcion para los arcosXhabitaciones redy

--- tarea2.cpp
+++ tarea2.cpp
@@ -12,17 +12,17 @@ struct Habitacion{
     string descripcion;
     string tipo;   
 
-    Habitacion* hijoIZQ = nullptr;
-    Habitacion* hijoMED = nullptr;
-    Habitacion* hijoDER = nullptr;
+    Habitacion* hijoIZQ = NULL;
+    Habitacion* hijoMED = NULL;
+    Habitacion* hijoDER = NULL;
 
 };
 
 
 
-Habitacion* leerHabitaciones(ifstream& fin, int& N){
+Habitacion* leerHabitaciones(ifstream& map, int& N){
     string linea;
-    if (!getline(fin, linea)) {
+    if (!getline(map, linea)) {
         cerr << "Error al leer el numero de habitaciones" << endl;
         exit(EXIT_FAILURE);
     }
@@ -31,7 +31,7 @@ Habitacion* leerHabitaciones(ifstream& fin, int& N){
     Habitacion* habitaciones = new Habitacion[N];
     for (int i = 0; i < N; ++i){
         do {
-            if (!getline(fin, linea)) {
+            if (!getline(map, linea)) {
                 cerr << "Error al leer la habitacion " << i + 1 << endl;
                 exit(EXIT_FAILURE);
             }
@@ -57,7 +57,7 @@ Habitacion* leerHabitaciones(ifstream& fin, int& N){
             }
         }
 
-        if (!getline(fin, descripcionEve)){
+        if (!getline(map, descripcionEve)){
             cerr << "Error al leer la descripcion de la habitacion " << i + 1 << endl;
             exit(EXIT_FAILURE);
         }
@@ -74,3 +74,49 @@ Habitacion* leerHabitaciones(ifstream& fin, int& N){
     return habitaciones;
 }
 
+
+void arcosXhabitaciones(ifstream& map, Habitacion* habitaciones, int N){
+    string linea;
+
+    if (!getline(map, linea)) {
+        cerr << "Error al leer los arcos" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    int ArcosTotales = stoi(linea);
+    for (int i = 0; i < ArcosTotales; ++i){
+        do {
+            if (!getline(map, linea)){
+                cerr << "Error al leer el arco" << i + 1 <<endl;
+                exit(EXIT_FAILURE)
+            }
+        } while (linea.empty());
+
+        size_t flechita = linea.find("->");
+        if (flechita == string::npos) {
+            cerr << "No se encontro la flechita -> " << i + 1 << endl;
+            exit(EXIT_FAILURE);
+        }
+
+        int altura = stoi(linea.substr(0, flechita));
+        int hijoEvento = stoi(linea.substr(flechita + 2));
+
+        Habitacion* padre = &habitaciones[altura];
+        Habitacion* hijo = &habitaciones[hijoEvento];
+
+        if (padre->hijoIZQ == NULL){
+            padre->hijoIZQ = hijo;        
+        }
+        else if (padre->hijoMED == NULL){
+            padre->hijoMED = hijo;
+        }
+        else if (padre->hijoDER == NULL){
+            padre->hijoDER = hijo;
+        }
+        else {
+            cerr << "Error: La habitacion " << padre->id << " ya tiene 3 hijos." << endl;
+            exit(EXIT_FAILURE);
+        }
+        
+    }
+}
